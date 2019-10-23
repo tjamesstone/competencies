d71e07885efa2cd20138e7e51c9759e26c0dac41
Tate Stone <tatejamesstone@gmail.com>
2019-10-23 14:32:58 -0600

    More work

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom'
-import App from './App'
+import Home from './Home'
 import Otherpage from './Otherpage'
 
 export default (
     <Switch>
-        <Route exact path='/' component={App}/>
+        <Route exact path='/' component={Home}/>
         <Route path='/otherpage' component={Otherpage}/>
+
     </Switch>
 )
